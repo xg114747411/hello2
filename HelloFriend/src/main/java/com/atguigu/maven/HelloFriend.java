84e5d9aa406fc5d3dd3f80821fe33a13f67bbb49
xugang <xg114747411@126.com>
2019-10-09 09:12:21 +0800

    初始化项目并提交

---
+++ HelloFriend/src/main/java/com/atguigu/maven/HelloFriend.java
@@ -0,0 +1,23 @@
+package com.atguigu.maven;
+
+public class HelloFriend {
+	/**
+	 * 在Maven工程中需要引用其他的jar包或者其他的项目
+	 * 	必须得通过坐标在maven仓库中获取
+	 * 	如果连接的是本地maven仓库，则在本地仓库中可以找到则可以去使用
+	 * 
+	 * 	- 如果引用的是自己创建的项目，必须先通过命令将项目安装到maven本地仓库中
+	 * @param name
+	 * @return
+	 */
+	public String sayHelloToFriend(String name){
+		Hello hello = new Hello();
+		String str = hello.sayHello(name)+" I am "+this.getMyName();
+		System.out.println(str);
+		return str;
+	}
+	public String getMyName(){
+		return "John";
+	}
+}
+
---
+++ HelloFriend/src/test/java/com/atguigu/maven/HelloFriendTest.java
@@ -0,0 +1,14 @@
+package com.atguigu.maven;
+
+import static junit.framework.Assert.assertEquals;
+import org.junit.Test;
+
+public class HelloFriendTest {
+	@Test
+	public void testHelloFriend(){
+		HelloFriend helloFriend = new HelloFriend();
+		String results = helloFriend.sayHelloToFriend("litingwei");
+		assertEquals("Hello litingwei! I am John",results);	
+		System.out.println("测试结果:"+results);
+	}
+}
